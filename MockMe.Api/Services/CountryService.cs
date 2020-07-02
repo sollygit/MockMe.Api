a6c96b367b9e9abccfc21ddc358ce377a8717ab2
Solly <mail4solly@gmail.com>
2020-07-02 23:29:43 +1000

    cache countries

--- MockMe.Api/Services/CountryService.cs
+++ MockMe.Api/Services/CountryService.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using MockMe.Common;
 using MockMe.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MockMe.Api.Services
@@ -15,26 +17,36 @@ namespace MockMe.Api.Services
     public class CountryService : ICountryService
     {
         private readonly ILogger<CountryService> _logger;
+        private readonly IMemoryCache _cache;
 
-        public CountryService(ILogger<CountryService> logger)
+        public CountryService(ILogger<CountryService> logger, IMemoryCache cache)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         public async Task<IEnumerable<Country>> GetAll()
         {
-            var countries = new List<Country>();
-
-            try
+            // Cache Countries for 24hrs due to high page load
+            return await _cache.GetOrCreateAsync("Countries", async e =>
             {
-                countries = MockUtil.Countries(5);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("CountryService GetAll Error", ex);
-            }
-
-            return await Task.FromResult(countries);
+                e.SetOptions(new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24)
+                });
+                
+                try
+                {
+                    var countries = await Task.FromResult(MockUtil.Countries(5));
+                    return countries.OrderBy(c => c.CountryName);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError("CountryService GetAll Error", ex);
+                }
+                
+                return Enumerable.Empty<Country>();
+            });
         }
     }
 }
--- MockMe.Api/Startup.cs
+++ MockMe.Api/Startup.cs
@@ -20,6 +20,7 @@ namespace MockMe.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddControllers();
             services.AddCors(options =>
             {
